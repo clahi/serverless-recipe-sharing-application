def454c9a71f6de1dc55bfd19003bbaf7a1e51ed
Abdala <clahimoha1000@gmail.com>
2025-07-18 15:38:12 +0300

    created a lambda function

---
+++ code/infrastructure/modules/backend/lambda/main.tf
@@ -0,0 +1,15 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = [ "sts:AssumeRole" ]
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}

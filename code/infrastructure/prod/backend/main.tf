27a967b66b745b19939c48967c32f10b386addb2
Abdala <clahimoha1000@gmail.com>
2025-07-22 17:18:40 +0300

    created a lambda function

--- code/infrastructure/prod/backend/main.tf
+++ code/infrastructure/prod/backend/main.tf
@@ -18,20 +18,19 @@ module "congnito" {
   email    = "clahimoha1000@gmail.com"
 }
 
-module "auth_lambda" {
-  source      = "../../modules/backend/lambda/auth"
-  environemnt = "prod"
-#   api_arn     = module.http_api.httpi_arn
-  api_execution_arn = module.http_api.execution_arn
-}
+# module "auth_lambda" {
+#   source      = "../../modules/backend/lambda/auth"
+#   environemnt = "prod"
+# #   api_arn     = module.http_api.httpi_arn
+#   api_execution_arn = module.http_api.execution_arn
+# }
 
 module "http_api" {
-  source                     = "../../modules/backend/HTTP-API"
-  stage_name                 = "prod"
-  pool_client                = module.congnito.user_poo_client_id
-#   cognito_pool_user_endpoint = module.congnito.cognito_endpoint
-cognito_issuer_url = module.congnito.cognito_issuer_url
-  lambda_auth_invoke_arn     = module.auth_lambda.lambda_auth_invoke_arn
+  source             = "../../modules/backend/HTTP-API"
+  stage_name         = "prod"
+  pool_client        = module.congnito.user_poo_client_id
+  cognito_issuer_url = module.congnito.cognito_issuer_url
+  environemnt        = "prod"
 }
 
 module "dynamoDB" {
--- code/infrastructure/prod/backend/outputs.tf
+++ code/infrastructure/prod/backend/outputs.tf
@@ -1,9 +1,9 @@
 output "http_endpoint" {
-  value = module.http_api.http_enpoint
+  value       = module.http_api.http_enpoint
   description = "The URI of the API"
 }
 
 output "api_gateway_integration_uri" {
-  value = module.http_api.api_gateway_integration_uri
+  value       = module.http_api.api_gateway_integration_uri
   description = "The integration URI for API Gateway"
 }
